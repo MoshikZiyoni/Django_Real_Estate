16b33ccdd796629d6cc62a2773a1481ffa98996a
גיא שטרצר <guystez@gmail.com>
2022-12-11 17:45:22 +0200

    heroku update

--- library/settings.py
+++ library/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$f5=8!v!l13idy6!#4_vhsy$4d^*40aj7g(+tuifuxj_v6@uu^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
@@ -42,13 +42,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'property',
-    'rest_framework'
+    'rest_framework',
+    'whitenoise.runserver_nostatic',
 
     
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
